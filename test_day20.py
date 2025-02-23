6c5d599bf23726e7ead49ec1bdbba50dd035a8f5
majd-badran-op <majd.badran@sociumtech.com>
2025-02-23 10:12:34 +0300

    add a muck test to test the api

---
+++ test_day20.py
@@ -0,0 +1,55 @@
+from day20 import get_post_by_id, get_posts_by_user_id, get_post_by_id_with_validation
+import unittest
+from unittest.mock import patch, Mock
+
+
+class TestDay20(unittest.TestCase):
+    def test_get_post_by_id_str_or_overflow_num(self):
+        self.assertIsNone(get_post_by_id('a'))
+        self.assertIsNone(get_post_by_id(1000000000000))
+
+    @patch('day20.http_get')
+    def test_get_post_by_id(self, mock_get):
+        mock_api = Mock()
+        mock_api.json.return_value = {
+            'userId': 1,
+            'id': 1,
+            'title': 'sunt aut facere repellat provident',
+            'body': 'quia et suscipit suscipit recusanda'
+        }
+        mock_get.return_value = mock_api
+        result = get_post_by_id(1)
+        self.assertEqual(result['id'], 1)
+        self.assertEqual(result['title'], 'sunt aut facere repellat provident')
+        self.assertEqual(result['body'], 'quia et suscipit suscipit recusanda')
+
+    @patch('day20.http_get')
+    def test_get_posts_by_user_id(self, mock_get):
+        mock_api = Mock()
+        mock_api.return_value.json.return_value = [{
+            'userId': 1,
+            'id': 2,
+            'title': 'sunt aut facere repellat provident',
+            'body': 'quia et suscipit suscipit recusanda'
+        }, {
+            'userId': 1,
+            'id': 3,
+            'title': 'sunt aut facere repellat provident',
+            'body': 'quia et suscipit suscipit recusanda'
+        }]
+        mock_get.return_value = mock_api
+        result1 = get_posts_by_user_id(1)
+        result2 = get_post_by_id_with_validation(1)
+        self.assertEqual(result1, mock_get.return_value.json.return_value)
+        self.assertEqual(result2, mock_get.return_value.json.return_value)
+
+    def test_get_post_by_id_with_validation(self):
+        with self.assertRaises(ValueError) as context:
+            get_post_by_id_with_validation(-1)
+        self.assertEqual(str(context.exception),
+                         'post_id must be greater than 0'
+                         )
+
+
+if __name__ == '__main__':
+    unittest.main()
